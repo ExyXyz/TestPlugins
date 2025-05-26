2d70c1cbadcd5338c46f4ec79b1fc23fc32a4e2d
Ekushi <108033300+ExyXyz@users.noreply.github.com>
2025-05-26 19:20:48 +0700

    Add files via upload

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,27 @@
-dependencies {
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
-}
 // use an integer for version numbers
-version = -1
+version = 20
 
 
 cloudstream {
+    language = "id"
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst")
+    // description = "Lorem Ipsum"
+     authors = listOf("Hexated")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
-    status = 1
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+    )
 
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // random cc logo i found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        viewBinding = true
-    }
+    iconUrl = "https://www.google.com/s2/favicons?domain=45.12.2.2&sz=%size%"
 }
